0d4052ee6a801bc2e291231903a7ad03c7794040
Renan Felipe de Andrade <renanfelipe2210@gmail.com>
2026-04-10 19:22:29 -0300

    Implement polynomial operations in operacoes.hpp
    Added functions for polynomial operations including addition, subtraction, scalar multiplication, and evaluation.

--- operacoes.hpp
+++ operacoes.hpp
@@ -1,1 +1,47 @@
+#include <iostream>
+#include <cmath>
+#include "polinomio.hpp"
+using namespace std;
 
+Polinomio somar(Polinomio* p1, Polinomio* p2)
+{
+    Termo* t1 = p1->primeiro;
+    Termo* t2 = p2->primeiro;
+    Termo* t3 = nullptr;
+
+    while(t1 != nullptr && t2 != nullptr)
+    {
+        if(t1->expoente == t2->expoente)
+        {
+            t3->coeficiente = t1->coeficiente + t2->coeficiente;
+        }
+
+        if(t1->expoente > t2->expoente)
+        {
+            t3->coeficiente = t1->coeficiente;
+            t3->expoente =
+        }
+        t1 = t1->prox;
+        t2 = t2->prox;
+    }
+}
+
+Polinomio subtrair(Polinomio* p1, Polinomio* p2)
+{
+
+}
+
+Polinomio multiplicarEscalar(Polinomio* p, double k)
+{
+
+}
+
+double valorNumerico(Polinomio* p, double x)
+{
+
+}
+
+Polinomio multiplicar(Polinomio* p1, Polinomio* p2)
+{
+
+}
