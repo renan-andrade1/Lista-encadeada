1eaa0eceaa5ca615cfbdf82084c5605a7cc0948f
Renan Felipe de Andrade <renanfelipe2210@gmail.com>
2026-04-10 20:08:49 -0300

    Refactor Polinomio functions to use references

--- polinomio.hpp
+++ polinomio.hpp
@@ -1,42 +1,89 @@
+#ifndef POLINOMIO_HPP
+#define POLINOMIO_HPP
+
 #include <iostream>
-#include "termo.hpp"
 using namespace std;
+
+struct Termo{
+    double coeficiente;
+    int expoente;
+    Termo* prox;
+};
 
 struct Polinomio{
     Termo* primeiro;
 };
 
-void inicializar(Polinomio* polinomio)
+void inicializar(Polinomio &polinomio)
 {
-    polinomio->primeiro = nullptr;
+    polinomio.primeiro = nullptr;
 }
 
-void inserir(Polinomio* polinomio, double coeficiente, int expoente)
+void inserirOrdenado(Polinomio &polinomio, double coeficiente, int expoente)
 {
+    if (coeficiente == 0.0) 
+    {
+        return;
+    }
+
     Termo* novo = new Termo;
     novo->coeficiente = coeficiente;
     novo->expoente = expoente;
     novo->prox = nullptr;
 
-    if (polinomio->primeiro == nullptr || polinomio->primeiro->expoente < expoente)
+    if (polinomio.primeiro == nullptr || polinomio.primeiro->expoente < expoente)
     {
-        novo->prox = polinomio->primeiro;
-        polinomio->primeiro = novo;
+        novo->prox = polinomio.primeiro;
+        polinomio.primeiro = novo;
         return;
     }
 
-    Termo* atual = polinomio->primeiro;
+    if(polinomio.primeiro->expoente == expoente)
+    {
+        polinomio.primeiro->coeficiente += coeficiente;
+        return;
+    }
+
+    Termo* atual = polinomio.primeiro;
 
     
     while (atual->prox != nullptr && atual->prox->expoente > expoente) 
     {
         atual = atual->prox;      
     }
+
     if(atual->prox != nullptr && atual->prox->expoente == expoente)
     {
         atual->prox->coeficiente += coeficiente;
         delete novo;
         return;
+    }
+
+    novo->prox = atual->prox;
+    atual->prox = novo;
+}
+
+void inserirFinal(Polinomio &polinomio, double coeficiente, int expoente) 
+{
+    if (coeficiente == 0.0) return;
+
+    Termo* novo = new Termo;
+    novo->coeficiente = coeficiente;
+    novo->expoente = expoente;
+    novo->prox = nullptr;
+
+    if (polinomio.primeiro == nullptr)
+    {
+        novo->prox = polinomio.primeiro;
+        polinomio.primeiro = novo;
+        return;
+    }
+
+    Termo* atual = polinomio.primeiro;
+
+    while (atual->prox != nullptr) 
+    {
+        atual = atual->prox;     
     }
 
     novo->prox = atual->prox;
@@ -104,3 +151,5 @@ void imprimir(Polinomio* polinomio)
     cout << endl;
 
 }
+
+#endif
